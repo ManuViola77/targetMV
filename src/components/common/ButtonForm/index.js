341cfcccb62c255a913cdb8818a190217acc28a2
Manuela Viola <manuelaviola@Manuelas-Mini.localdomain>
2020-01-24 16:57:21 -0300

    Pass props to ButtonForm

--- src/components/common/ButtonForm/index.js
+++ src/components/common/ButtonForm/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-const ButtonForm = ({ title }) => (
+const ButtonForm = ({ title, ...props }) => (
   <View style={styles.container}>
-    <TouchableOpacity style={styles.button}>
+    <TouchableOpacity style={styles.button} {...props}>
       <Text style={styles.text}> {title} </Text>
     </TouchableOpacity>
   </View>
