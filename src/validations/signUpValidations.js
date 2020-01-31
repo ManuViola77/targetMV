4b9ebd38fb265120f9289c83c78fcb04b88fe9f6
Manuela Viola <manuelaviola@Manuelas-Mini.localdomain>
2020-01-31 18:06:33 -0300

    Change creation of Object signUpValidations

--- src/validations/signUpValidations.js
+++ src/validations/signUpValidations.js
@@ -9,52 +9,52 @@ import {
 
 const { SIGN_UP_ERROR } = strings;
 
-const signUpValidations = {};
+const signUpValidations = {
+  [name]: {
+    presence: {
+      value: true,
+      message: SIGN_UP_ERROR.emptyName,
+    },
+  },
 
-signUpValidations[name] = {
-  presence: {
-    value: true,
-    message: SIGN_UP_ERROR.emptyName,
+  [email]: {
+    presence: {
+      value: true,
+      message: SIGN_UP_ERROR.emptyEmail,
+    },
+    email: {
+      value: true,
+      message: SIGN_UP_ERROR.emailNotValid,
+    },
   },
-};
 
-signUpValidations[email] = {
-  presence: {
-    value: true,
-    message: SIGN_UP_ERROR.emptyEmail,
+  [password]: {
+    presence: {
+      value: true,
+      message: SIGN_UP_ERROR.emptyPassword,
+    },
+    length: {
+      minimum: 6,
+      message: SIGN_UP_ERROR.passwordSixChar,
+    },
   },
-  email: {
-    value: true,
-    message: SIGN_UP_ERROR.emailNotValid,
-  },
-};
 
-signUpValidations[password] = {
-  presence: {
-    value: true,
-    message: SIGN_UP_ERROR.emptyPassword,
+  [confirmPassword]: {
+    presence: {
+      value: true,
+      message: SIGN_UP_ERROR.emptyConfirmPassword,
+    },
+    equality: {
+      attribute: password,
+      message: SIGN_UP_ERROR.confirmPasswordMatch,
+    },
   },
-  length: {
-    minimum: 6,
-    message: SIGN_UP_ERROR.passwordSixChar,
-  },
-};
 
-signUpValidations[confirmPassword] = {
-  presence: {
-    value: true,
-    message: SIGN_UP_ERROR.emptyConfirmPassword,
-  },
-  equality: {
-    attribute: password,
-    message: SIGN_UP_ERROR.confirmPasswordMatch,
-  },
-};
-
-signUpValidations[gender] = {
-  presence: {
-    value: true,
-    message: SIGN_UP_ERROR.emptyGender,
+  [gender]: {
+    presence: {
+      value: true,
+      message: SIGN_UP_ERROR.emptyGender,
+    },
   },
 };
 
