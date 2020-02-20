a62c872bbdb2aede31a861584cf40b2d0b21ba07
Manuela Viola <manuelaviola@MacBook-Pro.localdomain>
2020-02-20 16:06:37 -0300

    Fix Custom Marker in Map

--- src/components/common/Marker/index.js
+++ src/components/common/Marker/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Image, Platform } from 'react-native';
 import { Avatar } from 'react-native-elements';
-import MapView, { Marker as MapMarker } from 'react-native-maps';
+import { Circle, Marker as MapMarker } from 'react-native-maps';
 import { bool, number, string, shape } from 'prop-types';
 
 import { WHITE, YELLOW, YELLOW_TRANSPARENT } from 'constants/colors';
+import { IOS } from 'constants/common';
 import { CIRCLE_RADIUS, CIRCLE_BORDER_WIDTH } from 'constants/map';
 import styles from './styles';
 
@@ -16,41 +17,56 @@ const Marker = ({
   showCircle,
   uriIcon,
   radius,
-}) => (
-  <>
-    <MapMarker
-      key={markerKey}
-      coordinate={
-        uriIcon
-          ? { ...location, latitude: location.latitude - 0.00035 }
-          : location
-      }
-      onLoad={() => this.forceUpdate()}
-      draggable
-    >
-      {uriIcon ? (
-        <Avatar
-          size={25}
-          rounded
-          source={{ uri: uriIcon }}
-          overlayContainerStyle={styles.uriIconContainer}
-          activeOpacity={0.7}
+}) => {
+  const circle = useRef(null);
+
+  /*
+  This is a temporary workaround for the following issue in react-native-maps:
+  https://github.com/react-native-community/react-native-maps/issues/2698.
+  */
+  useEffect(() => {
+    if (Platform.OS === IOS && circle && circle.current) {
+      circle.current.setNativeProps({
+        fillColor: uriIcon ? YELLOW_TRANSPARENT : WHITE,
+        strokeColor: YELLOW,
+      });
+    }
+  }, [circle]);
+
+  return (
+    <>
+      <MapMarker
+        key={markerKey}
+        coordinate={location}
+        onLoad={() => this.forceUpdate()}
+        draggable
+        anchor={uriIcon ? { x: 0.5, y: 0.5 } : { x: 0.5, y: 1 }}
+      >
+        {uriIcon ? (
+          <Avatar
+            size={25}
+            rounded
+            source={{ uri: uriIcon }}
+            overlayContainerStyle={styles.uriIconContainer}
+            activeOpacity={0.7}
+          />
+        ) : (
+          <Image source={icon} />
+        )}
+      </MapMarker>
+      {showCircle && (
+        <Circle
+          ref={circle}
+          center={location}
+          radius={uriIcon ? radius : CIRCLE_RADIUS}
+          strokeWidth={uriIcon ? 0 : CIRCLE_BORDER_WIDTH}
+          strokeColor={YELLOW}
+          fillColor={uriIcon ? YELLOW_TRANSPARENT : WHITE}
         />
-      ) : (
-        <Image source={icon} />
       )}
-    </MapMarker>
-    {showCircle && (
-      <MapView.Circle
-        center={location}
-        radius={uriIcon ? radius : CIRCLE_RADIUS}
-        strokeWidth={uriIcon ? 0 : CIRCLE_BORDER_WIDTH}
-        strokeColor={YELLOW}
-        fillColor={uriIcon ? YELLOW_TRANSPARENT : WHITE}
-      />
-    )}
-  </>
-);
+    </>
+  );
+};
 
 Marker.propTypes = {
   draggable: bool,
